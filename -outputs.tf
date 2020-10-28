4eca578f0b1a8ea535224ad8cd576c0ce487c303
Chris Hurst <chris.hurst@stratusgrid.com>
2020-10-28 18:26:38 -0400

    added output for ecs cluster name

--- -outputs.tf
+++ -outputs.tf
@@ -1,3 +1,8 @@
+output "ecs_cluster_name" {
+  description = "ARN of ECS cluster created by this module."
+  value = aws_ecs_cluster.this.name
+}
+
 output "ecs_cluster_arn" {
   description = "ARN of ECS cluster created by this module."
   value = aws_ecs_cluster.this.arn
