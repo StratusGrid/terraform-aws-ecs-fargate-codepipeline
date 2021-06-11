0225cbf03a7d3fa84c2c55bf04a8ba1fe17ed3bd
Ivan Casco <ivan.casco@stratusgrid.com>
2021-06-11 03:18:10 +0200

    Add output codedeploy_app_arns_map

--- -outputs.tf
+++ -outputs.tf
@@ -7,3 +7,10 @@ output "ecs_cluster_arn" {
   description = "ARN of ECS cluster created by this module."
   value = aws_ecs_cluster.this.arn
 }
+
+output "codedeploy_app_arns_map" {
+  description = "Map of ARNs of CodeDeploy app created by this module."
+  value = tomap({
+    for k, service in aws_codedeploy_app.this : k => service.arn
+  })
+}
