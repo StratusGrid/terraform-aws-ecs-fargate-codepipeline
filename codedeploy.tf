4c5e4a8160f264d0861535a92c7a972f9b1975e1
Matt Barlow <83474131+mattbarlow-sg@users.noreply.github.com>
2021-09-21 14:38:45 -0500

    Deprecate termination_wait_time (#7)

--- codedeploy.tf
+++ codedeploy.tf
@@ -25,7 +25,7 @@ resource "aws_codedeploy_deployment_group" "this" {
 
     terminate_blue_instances_on_deployment_success {
       action                           = "TERMINATE"
-      termination_wait_time_in_minutes = var.termination_wait_time
+      termination_wait_time_in_minutes = var.ecs_services[each.key].codedeploy_termination_wait_time
     }
   }
 
