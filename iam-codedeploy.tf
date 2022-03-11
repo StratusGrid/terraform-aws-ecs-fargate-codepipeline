4359c6a6481eecda5700f854f339c467e17b1f9d
Chris Childress <chris.childress@stratusgrid.com>
2022-03-10 22:05:48 -0500

    refactor: use jsonencode instead of heredoc

--- iam-codedeploy.tf
+++ iam-codedeploy.tf
@@ -28,12 +28,12 @@ resource "aws_iam_role_policy" "this_codedeploy" {
   name = var.service_name
   role = aws_iam_role.this_codedeploy.id
 
-  policy = <<POLICY
+  policy = jsonencode(
 {
-    "Version": "2012-10-17",
-    "Statement": [
+    "Version"= "2012-10-17"
+    "Statement"= [
         {
-            "Action": [
+            "Action"= [
                 "ecs:DescribeServices",
                 "ecs:CreateTaskSet",
                 "ecs:UpdateServicePrimaryTaskSet",
@@ -49,26 +49,57 @@ resource "aws_iam_role_policy" "this_codedeploy" {
                 "s3:GetObject",
                 "s3:GetObjectVersion"
             ],
-            "Resource": "*",
-            "Effect": "Allow"
+            "Resource"= "*",
+            "Effect"= "Allow"
         },
         {
-            "Action": [
+            "Action"= [
                 "iam:PassRole"
             ],
-            "Effect": "Allow",
-            "Resource": "*",
-            "Condition": {
-                "StringLike": {
-                    "iam:PassedToService": [
+            "Effect"= "Allow",
+            "Resource"= "*",
+            "Condition"= {
+                "StringLike"= {
+                    "iam:PassedToService"= [
                         "ecs-tasks.amazonaws.com"
                     ]
                 }
             }
-        }
+        },
+      {
+        Sid     = "CrossAccountBucketAccess"
+        Effect  = "Allow"
+        Actions = [
+          "s3:Get*"
+        ]
+        Resources = [
+          "${data.aws_s3_bucket.cicd_artifacts.arn}/*"
+        ]
+      },
+      {
+        Sid     = "CrossAccountBucketListAccess"
+        Effect = "Allow"
+        Actions = ["s3:ListBucket"]
+        Resources = [
+          data.aws_s3_bucket.cicd_artifacts.arn
+        ]
+      },
+      {
+        Sid     = "CrossAccountKMSAccess"
+        Effect  = "Allow"
+        Actions = [
+          "kms:DescribeKey",
+           "kms:GenerateDataKey*",
+           "kms:Encrypt",
+           "kms:ReEncrypt*",
+           "kms:Decrypt"
+        ]
+        Resources = [
+          data.aws_kms_key.cicd_encryption_key.arn
+        ]
+      }
     ]
-}
-POLICY
+})
 }
 
 resource "aws_iam_role_policy_attachment" "codedeploy_role_additional_policies" {
