f49419d21b3cc63bd14104968bd1c52b5dc561aa
DJKaswa <dieudonne.kaswa@stratusgrid.com>
2022-09-19 16:11:40 -0500

    Fixes typo.

--- ecs-cluster.tf
+++ ecs-cluster.tf
@@ -1,5 +1,5 @@
 # cluster
-#tfsec:ignore:aws-ecs-enable-container-insight -- Ingore the error about container logs not collecting all metric,
+#tfsec:ignore:aws-ecs-enable-container-insight -- Ingore the error about container logs not collecting all metrics
 resource "aws_ecs_cluster" "this" {
   name               = var.ecs_cluster_name
   tags               = merge(var.input_tags, {})
